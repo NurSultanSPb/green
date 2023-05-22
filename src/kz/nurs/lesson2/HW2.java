e6de8a88238ebec82a83eca5314125fe90dfe32a
NurSultan <tnursi9898g@gmail.com>
2023-05-22 18:28:02 +0300

    Task 2 is done

---
+++ src/kz/nurs/lesson2/HW2.java
@@ -0,0 +1,10 @@
+package kz.nurs.lesson2;
+
+public class HW2 {
+    public static void main(String[] args) {
+
+    }
+    private static void task2() {
+        System.out.println("Выполнил таск 2");
+    }
+}
