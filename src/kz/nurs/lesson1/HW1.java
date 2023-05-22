9e32050d1e5a962f321b69ad78493f9e2b6fa44b
NurSultan <tnursi9898g@gmail.com>
2023-05-22 18:04:30 +0300

    First task fixed!

--- src/kz/nurs/lesson1/HW1.java
+++ src/kz/nurs/lesson1/HW1.java
@@ -5,6 +5,6 @@ public class HW1 {
         task1();
     }
     private static void task1() {
-        System.out.println("Решение первой задачи");
+        System.out.println("Решение первой задачи!!!");
     }
 }
